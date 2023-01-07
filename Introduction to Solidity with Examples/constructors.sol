22925709f4024091e7bb6057301e327147c5c7c2
JanusZhu <54954440+JanusZhu@users.noreply.github.com>
2023-01-07 18:45:46 -0500

    Update constructors.sol

--- Introduction to Solidity with Examples/constructors.sol
+++ Introduction to Solidity with Examples/constructors.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+contract A {
+    uint public a;
+    uint public x;
+    uint public y;
+    address public owner;
+    uint public createdAt;
+
+//use the constructor below to set variables at time of deployment
+//pass in a value for x and y into the constructor to set the state variable
+//the owner of the contract is the msg.sender (the contract creator)
+//the createdAt state variable will contain the block time stamp at when the constructor was created
+    constructor (uint _x, uint _y) {
+            x=_x;
+            y=_y;
+            owner = msg.sender;
+            createdAt = block.timestamp;
+
+    }
+}
